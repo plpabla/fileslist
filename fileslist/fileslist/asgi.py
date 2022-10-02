24dc6ea03c8e554bef347f18964fa104f536015f
pawel-laptop <p.blaszczy@gmail.com>
2022-10-02 19:38:51 +0200

    Create empty Django project

--- fileslist/fileslist/asgi.py
+++ fileslist/fileslist/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for filelists project.
+ASGI config for fileslist project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "filelists.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fileslist.settings")
 
 application = get_asgi_application()
--- fileslist/fileslist/wsgi.py
+++ fileslist/fileslist/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for filelists project.
+WSGI config for fileslist project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "filelists.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fileslist.settings")
 
 application = get_wsgi_application()
