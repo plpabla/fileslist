63622dc5f9d8c48f92488f148d245d68c1276a99
pawel-laptop <p.blaszczy@gmail.com>
2022-10-12 19:07:44 +0200

    Failing test - no reverse url for "home"

--- fileslist/features/steps/common_steps.py
+++ fileslist/features/steps/common_steps.py
@@ -1,13 +1,26 @@
+import time
 from behave import step
+from selenium import webdriver
+from django.urls import reverse
+
+def resolve_url(url, root=None):
+    if root is None:
+        return url
+
+    full_url = root + reverse(url) 
+    return full_url   
 
 @step("opened browser")
 def my_step(context):
-    pass
-
+    opts = webdriver.FirefoxOptions()
+    # opts.add_argument("--headless")
+    context.browser = webdriver.Firefox(options=opts)
+    # context.browser.implicitly_wait(3)
+    
 @step("I'm on the '{url}' page")
 def my_step(context, url):
-    pass
+    context.browser.get(resolve_url(url, root=context.test.live_server_url))
 
 @step("Page title contains '{expected_title}'")
 def my_step(context, expected_title):
-    pass
+    context.browser.quit()
