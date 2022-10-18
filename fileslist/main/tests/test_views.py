722914bd2f33270dcb468490fd2ec4979da34d23
pawel-laptop <p.blaszczy@gmail.com>
2022-10-18 21:26:04 +0200

    Add test to check if page contains FIlesList

--- fileslist/main/tests/test_views.py
+++ fileslist/main/tests/test_views.py
@@ -1,6 +1,13 @@
+from django.http import HttpResponse
 from django.urls import reverse
 import logging
 from pytest_django.asserts import assertTemplateUsed
+
+
+import re
+def assert_title_contains(response: HttpResponse, title:str) -> bool:
+    regexp = re.compile(r"<title>.*%s.*</title>" % (title))
+    assert regexp.search(response.content.decode()), f"'{title}' not found in the page title"
 
 def test_root_url_resolves_to_home_page():
     page_found = reverse("home")
@@ -13,3 +20,7 @@ def test_home_page_renders_home_template(client):
     
     assertTemplateUsed(res, "home.html")
     
+def test_home_page_has_given_title(client):
+    res = client.get(reverse("home"))
+
+    assert_title_contains(res, "FilesList")
