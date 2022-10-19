2522208001394a2f77f942b359d31771626fc540
pawel-laptop <p.blaszczy@gmail.com>
2022-10-19 23:10:20 +0200

    Refactor test_views
    Move assertion to custom_asserts

---
+++ fileslist/main/tests/custom_asserts.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+import re
+
+def assert_title_contains(response: HttpResponse, title:str) -> bool:
+    regexp = re.compile(r"<title>.*%s.*</title>" % (title))
+    assert regexp.search(response.content.decode()), f"'{title}' not found in the page title"
--- fileslist/main/tests/test_views.py
+++ fileslist/main/tests/test_views.py
@@ -1,25 +1,21 @@
-from django.http import HttpResponse
 from django.urls import reverse
 import logging
 from pytest_django.asserts import assertTemplateUsed
+from custom_asserts import assert_title_contains
 
-
-import re
-def assert_title_contains(response: HttpResponse, title:str) -> bool:
-    regexp = re.compile(r"<title>.*%s.*</title>" % (title))
-    assert regexp.search(response.content.decode()), f"'{title}' not found in the page title"
 
 def test_root_url_resolves_to_home_page():
     page_found = reverse("home")
 
     assert page_found == r"/"
 
+
 def test_home_page_renders_home_template(client):
     res = client.get(reverse("home"))
-    logging.info(f"\nHome page response: {res.content.decode()}")
     
     assertTemplateUsed(res, "home.html")
-    
+
+
 def test_home_page_has_given_title(client):
     res = client.get(reverse("home"))
 
